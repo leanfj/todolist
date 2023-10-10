db797c4ea3de25618f89dbd4044942ddbb30ec4b
Leandro Ferreira <leanfj@hotmail.com>
2023-10-09 21:54:38 -0300

    Commit inicial

---
+++ src/main/java/br/com/leanfj/todolist/user/UserModel.java
@@ -0,0 +1,19 @@
+package br.com.leanfj.todolist.user;
+
+public class UserModel {
+    private String userName;
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    public String getUserName() {
+        return userName;
+    }
+}
